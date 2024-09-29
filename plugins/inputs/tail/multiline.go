2701798d3fb3987e1fdf494343477ee0c40773fa
Yun Long <longdeqidao@gmail.com>
2024-09-29 15:58:39 +0800

    Update inputs.tail.multiline to support replace_pattern, first_pattern and end_pattern

--- plugins/inputs/tail/multiline.go
+++ plugins/inputs/tail/multiline.go
@@ -14,15 +14,25 @@ import (
 type MultilineMatchWhichLine int
 
 type Multiline struct {
-	config        *MultilineConfig
-	enabled       bool
-	patternRegexp *regexp.Regexp
-	quote         byte
-	inQuote       bool
+	config               *MultilineConfig
+	enabled              bool
+	replacePatternRegexp *regexp.Regexp
+	startPatternRegexp   *regexp.Regexp
+	endPatternRegexp     *regexp.Regexp
+	patternRegexp        *regexp.Regexp
+	quote                byte
+	inQuote              bool
+
+	startedMatch bool
 }
 
 type MultilineConfig struct {
+	ReplacePattern string `toml:"replace_pattern"`
+	Replacement    string `toml:"replacement"`
+
+	StartPattern    string                  `toml:"start_pattern"`
 	Pattern         string                  `toml:"pattern"`
+	EndPattern      string                  `toml:"end_pattern"`
 	MatchWhichLine  MultilineMatchWhichLine `toml:"match_which_line"`
 	InvertMatch     bool                    `toml:"invert_match"`
 	PreserveNewline bool                    `toml:"preserve_newline"`
@@ -38,11 +48,43 @@ const (
 )
 
 func (m *MultilineConfig) NewMultiline() (*Multiline, error) {
-	var r *regexp.Regexp
+	var replacePatternRegexp *regexp.Regexp
+	var startPatternRegexp *regexp.Regexp
+	var endPatternRegexp *regexp.Regexp
+	var patternRegexp *regexp.Regexp
+
+	if m.ReplacePattern != "" {
+		var err error
+		if replacePatternRegexp, err = regexp.Compile(m.ReplacePattern); err != nil {
+			return nil, errors.New("replace_pattern is invalid")
+		}
+	}
+
+	if m.StartPattern != "" {
+		if m.EndPattern == "" {
+			return nil, errors.New("start_pattern is set, but end_pattern is not")
+		}
+
+		var err error
+		if startPatternRegexp, err = regexp.Compile(m.StartPattern); err != nil {
+			return nil, err
+		}
+	}
+
+	if m.EndPattern != "" {
+		if m.StartPattern == "" {
+			return nil, errors.New("end_pattern is set, but start_pattern is not")
+		}
+
+		var err error
+		if endPatternRegexp, err = regexp.Compile(m.EndPattern); err != nil {
+			return nil, err
+		}
+	}
 
 	if m.Pattern != "" {
 		var err error
-		if r, err = regexp.Compile(m.Pattern); err != nil {
+		if patternRegexp, err = regexp.Compile(m.Pattern); err != nil {
 			return nil, err
 		}
 	}
@@ -61,17 +103,20 @@ func (m *MultilineConfig) NewMultiline() (*Multiline, error) {
 		return nil, errors.New("invalid 'quotation' setting")
 	}
 
-	enabled := m.Pattern != "" || quote != 0
+	enabled := m.Pattern != "" || quote != 0 || m.StartPattern != "" || m.EndPattern != "" || m.ReplacePattern != ""
 	if m.Timeout == nil || time.Duration(*m.Timeout).Nanoseconds() == int64(0) {
 		d := config.Duration(5 * time.Second)
 		m.Timeout = &d
 	}
 
 	return &Multiline{
-		config:        m,
-		enabled:       enabled,
-		patternRegexp: r,
-		quote:         quote,
+		config:               m,
+		enabled:              enabled,
+		replacePatternRegexp: replacePatternRegexp,
+		startPatternRegexp:   startPatternRegexp,
+		endPatternRegexp:     endPatternRegexp,
+		patternRegexp:        patternRegexp,
+		quote:                quote,
 	}, nil
 }
 
@@ -80,6 +125,53 @@ func (m *Multiline) IsEnabled() bool {
 }
 
 func (m *Multiline) ProcessLine(text string, buffer *bytes.Buffer) string {
+	if m.replacePatternRegexp != nil {
+		text = m.replacePatternRegexp.ReplaceAllString(text, m.config.Replacement)
+	}
+
+	if m.startPatternRegexp != nil && m.endPatternRegexp != nil {
+		if m.startedMatch {
+			if m.endPatternRegexp.MatchString(text) {
+				// End pattern found, flush the buffer.
+				if m.config.PreserveNewline {
+					buffer.WriteString("\n")
+				}
+				buffer.WriteString(text)
+				text = buffer.String()
+				buffer.Reset()
+
+				m.startedMatch = false
+
+				// fmt.Printf("### match %s : %s\n\n", m.endPatternRegexp.String(), text)
+
+				return text
+			}
+
+			// Just collect the text between start and end patterns.
+			if buffer.Len() > 0 && m.config.PreserveNewline {
+				buffer.WriteString("\n")
+			}
+			buffer.WriteString(text)
+			return ""
+		}
+
+		// fmt.Printf("--- match %s : %s\n", m.startPatternRegexp.String(), text)
+		// fmt.Printf("--- result: %v\n\n", m.startPatternRegexp.MatchString(text))
+		// Start pattern found, start collecting the text.
+		if m.startPatternRegexp.MatchString(text) {
+			m.startedMatch = true
+
+			previousText := buffer.String()
+			buffer.Reset()
+			buffer.WriteString(text)
+
+			return previousText
+		}
+
+		// if we are not in a match, just return the text
+		return text
+	}
+
 	if m.matchQuotation(text) || m.matchString(text) {
 		// Restore the newline removed by tail's scanner
 		if buffer.Len() > 0 && m.config.PreserveNewline {
@@ -144,6 +236,22 @@ func (m *Multiline) matchQuotation(text string) bool {
 	even := count%2 == 0
 	m.inQuote = (m.inQuote && even) || (!m.inQuote && !even)
 	return m.inQuote
+}
+
+func (m *Multiline) matchStartLine(text string) bool {
+	if m.startPatternRegexp != nil {
+		// start pattern does not consider invert match option.
+		return m.startPatternRegexp.MatchString(text)
+	}
+	return false
+}
+
+func (m *Multiline) matchEndLine(text string) bool {
+	if m.endPatternRegexp != nil {
+		// end pattern does not consider invert match option.
+		return m.endPatternRegexp.MatchString(text)
+	}
+	return false
 }
 
 func (m *Multiline) matchString(text string) bool {
